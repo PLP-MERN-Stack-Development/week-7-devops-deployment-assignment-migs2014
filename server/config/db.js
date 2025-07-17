afad7cf15e2239b5d2ea4794a354f6a6936e17ec
migs2014 <migeledouglas@gmail.com>
2025-07-17 20:18:11 +0300

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,31 +1,11 @@
-// import mongoose from 'mongoose';
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI_PRODUCTION,);
-//     console.log('MongoDB connected');
-//   } catch (err) {
-//     console.error(err.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI_PRODUCTION || 'mongodb://localhost:27017/blog-app';
-    console.log('Mongo URI:', mongoURI); // Optional: remove in production
-
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    await mongoose.connect(process.env.MONGO_URI_PRODUCTION,);
     console.log('MongoDB connected');
   } catch (err) {
-    console.error('MongoDB connection error:', err.message);
+    console.error(err.message);
     process.exit(1);
   }
 };
