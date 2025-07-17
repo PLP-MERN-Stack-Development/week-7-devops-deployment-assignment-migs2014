ca277c46a9ef8ad688ae8cd188e3aecd0e6f4371
migs2014 <migeledouglas@gmail.com>
2025-07-17 18:06:24 +0300

    uri

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_PRODUCTION);
+    await mongoose.connect(process.env.MONGO_URI_PRODUCTION,);
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
