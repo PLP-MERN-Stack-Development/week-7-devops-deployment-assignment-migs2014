047db08cf84114307a403be8b0c0d12067e8cb92
migs2014 <migeledouglas@gmail.com>
2025-07-18 15:08:39 +0300

    base Url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://week-7-devops-deployment-assignment-e7f5.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
